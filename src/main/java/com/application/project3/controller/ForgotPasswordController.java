a48b6d3790ae812d70dcbb7b4eda1c2bd7040504
Matheus <matheusamrl@gmail.com>
2021-06-21 13:59:06 -0300

    Errors

--- src/main/java/com/application/project3/controller/ForgotPasswordController.java
+++ src/main/java/com/application/project3/controller/ForgotPasswordController.java
@@ -6,14 +6,12 @@ import com.application.project3.entities.ValidateForm;
 import com.application.project3.repository.UserRepository;
 import com.application.project3.security.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
-import java.util.List;
+
 
 @Controller
 @SessionAttributes({"validateForm"})
