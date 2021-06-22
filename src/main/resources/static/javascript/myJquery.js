6f9a025b8f617547875d10481abb159b1752e8fe
Matheus <matheusamrl@gmail.com>
2021-06-22 11:46:14 -0300

    Errors

--- src/main/resources/static/javascript/myJquery.js
+++ src/main/resources/static/javascript/myJquery.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
                     flag = false;
                 }
                 else {
-                    let patt = /[A-Za-z]{6,10}/;
+                    let patt = /[A-Za-z0-9]{6,10}/;
                     if(!patt.test($("#userId").val())) {
                         message += "User Id must be alphanumeric and should contain 6 - 10 character <br>";
                         flag = false;
@@ -65,8 +65,8 @@ $(document).ready(function() {
                     flag = false;
                 }
                 else {
-                    let patt = /(1[89]|[2-9][0-9]|1[01][0-9]|120)/;
-                    if(!patt.test($("#age").val())) {
+                    let patt = parseInt($("#age").val());
+                    if(patt < 18 || patt > 120) {
                         message += "Age should be greater than 18 and less than 120 <br>";
                         flag = false;
                     }
@@ -83,5 +83,45 @@ $(document).ready(function() {
             });
         }
 
+         if($("#forgotpassword")) {
+                    $("#forgotpassword").click(function() {
+                        let flag = true;
+                        let message = "";
+
+                        if($("#userId").val() == "") {
+                            message += "User Id is Mandatory <br>" ;
+                            flag = false;
+                        }
+                        else {
+                            let patt = /[A-Za-z0-9]{6,10}/;
+                            if(!patt.test($("#userId").val())) {
+                                message += "User Id must be alphanumeric and should contain 6 - 10 character <br>";
+                                flag = false;
+                            }
+                        }
+
+                              if($("#petName").val() == "") {
+                                            message += "Pet name is Mandatory <br>" ;
+                                            flag = false;
+                                        }
+                                        else {
+                                            let patt = /[A-Za-z]{3,10}/;
+                                            if(!patt.test($("#petName").val())) {
+                                                message += "Pet Name must be at least 3 - 10 characters long <br>";
+                                                flag = false;
+                                            }
+                                        }
+
+
+
+                      if(flag) {
+                                        $("#validateForm").submit();
+                                    }
+                                    else {
+                                         $("#error").html(message);
+                                         return false;
+                                    }
+                                });
+                            }
 
     });
